3a2dbc928c27b7890876998bfa92cb468032fd91
Kushal Rathod <kushalrathod044@gmail.com>
2026-04-17 15:19:27 +0530

    add: sign up page

---
+++ app/(routes)/auth/signup/page.tsx
@@ -0,0 +1,14 @@
+import { SignupForm } from "@/components/app-components/auth/signup/signup";
+
+
+const Signup = () => {
+    return (
+        <div className="flex-1 flex justify-center items-center">
+            <div className="w-full max-w-sm">
+                <SignupForm />
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
