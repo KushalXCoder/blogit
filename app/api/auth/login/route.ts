afef4a3e6ed842c9cf5f651791d0872b93d37071
Kushal Rathod <kushalrathod044@gmail.com>
2026-04-20 09:29:53 +0530

    add: implement login and signup routes with token handling

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,46 @@
+import { checkPassword } from "@/lib/checkPassoword";
+import { connectDb } from "@/lib/drivers/db";
+import { User } from "@/models/user.model";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: NextResponse) => {
+    try {
+        // Extract email and password from the request body
+        const { email, password } = await req.json();
+        if(!email || !password) {
+            return NextResponse.json({ message: "Email and password are required" }, { status: 400 });
+        }
+
+        // Connect to the database
+        await connectDb();
+
+        // Check if the user exists in the database
+        const user = await User.findOne({ email });
+        if(!user) {
+            return NextResponse.json({ message: "User not found" }, { status: 404 });
+        }
+
+        // Check the password using the bcrypt compare function
+        const isPasswordValid = await checkPassword(password, user.password);
+        if(!isPasswordValid) {
+            return NextResponse.json({ message: "Invalid password" }, { status: 401 });
+        }
+
+        // Create a token
+        const token = JSON.stringify({ username: user.username, email: user.email });
+
+        // Set the token in an HTTP-only cookie
+        const res = NextResponse.json({ message: "Login successful" }, { status: 200 });
+        res.cookies.set("indoc-token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            path: "/",
+            maxAge: 60 * 60 * 24 * 7
+        });
+
+        return res;
+    } catch (error) {
+        return NextResponse.json({ message: "Invalid request body", error: error instanceof Error ? error.message : "Unknown error" }, { status: 400 });
+    }
+}
