e7b882920a022b2f39e57d1e3d27972c57539f7f
Kushal Rathod <kushalrathod044@gmail.com>
2026-07-07 12:04:36 +0530

    add: loader component

---
+++ src/components/app-components/loader.tsx
@@ -0,0 +1,8 @@
+import { LoaderCircle } from "@hugeicons/core-free-icons"
+import { HugeiconsIcon } from "@hugeicons/react"
+
+export const Loader = () => {
+    return (
+        <HugeiconsIcon icon={LoaderCircle} className="mr-2 h-4 w-4 animate-spin shrink-0" />
+    )
+}
